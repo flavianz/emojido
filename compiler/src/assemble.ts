23e59c214831470d34832c6c08d027aeacecdd8c
flavi <flavianzuellig@hotmail.com>
2024-03-23 00:32:15 +0100

    try to adapt new http library

--- compiler/src/assemble.ts
+++ compiler/src/assemble.ts
@@ -1,50 +1,52 @@
-import axios from "redaxios";
+import got from "got";
 
 export async function execute(source: string) {
-    const result = await axios.post(
-        "https://godbolt.org/api/compiler/nasm21601/compile",
-        {
-            allowStoreDebug: true,
-            bypassCache: 0,
-            compiler: "nasm21601",
-            files: [],
-            lang: "assembly",
-            source: source,
-            options: {
-                userArguments: "-felf64",
-                compilerOptions: {
-                    producePp: null,
-                    produceGccDump: {},
-                    produceOptInfo: false,
-                    produceCfg: false,
-                    produceIr: null,
-                    produceOptPipeline: null,
-                    produceDevice: false,
-                    overrides: [],
-                },
-                filters: {
-                    binaryObject: false,
-                    binary: false,
-                    execute: true,
-                    intel: true,
-                    demangle: true,
-                    labels: true,
-                    directives: true,
-                    commentOnly: true,
-                    trim: false,
-                    debugCalls: false,
-                },
-                tools: [],
-                libraries: [],
-                executeParameters: {
-                    args: "",
-                    stdin: "",
+    const { data } = await got
+        .post("https://godbolt.org/api/compiler/nasm21601/compile", {
+            json: {
+                allowStoreDebug: true,
+                bypassCache: 0,
+                compiler: "nasm21601",
+                files: [],
+                lang: "assembly",
+                source: source,
+                options: {
+                    userArguments: "-felf64",
+                    compilerOptions: {
+                        producePp: null,
+                        produceGccDump: {},
+                        produceOptInfo: false,
+                        produceCfg: false,
+                        produceIr: null,
+                        produceOptPipeline: null,
+                        produceDevice: false,
+                        overrides: [],
+                    },
+                    filters: {
+                        binaryObject: false,
+                        binary: false,
+                        execute: true,
+                        intel: true,
+                        demangle: true,
+                        labels: true,
+                        directives: true,
+                        commentOnly: true,
+                        trim: false,
+                        debugCalls: false,
+                    },
+                    tools: [],
+                    libraries: [],
+                    executeParameters: {
+                        args: "",
+                        stdin: "",
+                    },
                 },
             },
-        },
-    );
+        })
+        .json();
 
     console.log("Standard Output:");
+    console.log(data);
     console.log(
         ...result.data.execResult.stdout.map((text) => {
             return text.text;
