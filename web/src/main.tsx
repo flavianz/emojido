dfb49e8cfd53e335a5647497a055af228f1ec4a6
Flavianthepavian <flavianzuellig@hotmail.com>
2024-03-22 12:51:57 +0100

    add home page

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -7,8 +7,12 @@ import {
     RouterProvider,
 } from "react-router-dom";
 import Run from "./sites/Run/Run.tsx";
+import Home from "./sites/Home/Home.tsx";
 
-const router = createHashRouter([{ path: "/run", element: <Run /> }]);
+const router = createHashRouter([
+    { path: "/run", element: <Run /> },
+    { path: "/", element: <Home /> },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
