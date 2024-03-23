9caf9bbdde5e2c00e11717840a6ef3bb38f43dd1
flavi <flavianzuellig@hotmail.com>
2024-03-23 23:13:39 +0100

    make responsive

--- web/src/sites/Error/Error.tsx
+++ web/src/sites/Error/Error.tsx
@@ -3,7 +3,8 @@ import styles from "./Error.module.css";
 export default function Error() {
     return (
         <div className={styles.container}>
-            <h1>404</h1>
+            <h1 className={styles.title}>404</h1>
+            <div className={styles.divider}></div>
             <div className={styles.helpContainer}>
                 <h2>Feeling lost?</h2>
                 <a href="/#/docs">See the docs</a>
