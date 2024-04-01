b6adb41939fcad52bad8c2490ad49007a3b5fadd
flavi <flavianzuellig@hotmail.com>
2024-04-01 22:53:29 +0200

    change from ky to axios only for local compiler

--- compiler/src/assemble.ts
+++ compiler/src/assemble.ts
@@ -1,49 +1,47 @@
-import ky from "ky";
+import { post } from "axios";
 
 export async function execute(source: string) {
-    const result = (await ky
-        .post("https://godbolt.org/api/compiler/nasm21601/compile", {
-            json: {
-                allowStoreDebug: true,
-                bypassCache: 0,
-                compiler: "nasm21601",
-                files: [],
-                lang: "assembly",
-                source: source,
-                options: {
-                    userArguments: "-felf64",
-                    compilerOptions: {
-                        producePp: null,
-                        produceGccDump: {},
-                        produceOptInfo: false,
-                        produceCfg: false,
-                        produceIr: null,
-                        produceOptPipeline: null,
-                        produceDevice: false,
-                        overrides: [],
-                    },
-                    filters: {
-                        binaryObject: false,
-                        binary: false,
-                        execute: true,
-                        intel: true,
-                        demangle: true,
-                        labels: true,
-                        directives: true,
-                        commentOnly: true,
-                        trim: false,
-                        debugCalls: false,
-                    },
-                    tools: [],
-                    libraries: [],
-                    executeParameters: {
-                        args: "",
-                        stdin: "",
-                    },
+    const result = (
+        await post("https://godbolt.org/api/compiler/nasm21601/compile", {
+            allowStoreDebug: true,
+            bypassCache: 0,
+            compiler: "nasm21601",
+            files: [],
+            lang: "assembly",
+            source: source,
+            options: {
+                userArguments: "-felf64",
+                compilerOptions: {
+                    producePp: null,
+                    produceGccDump: {},
+                    produceOptInfo: false,
+                    produceCfg: false,
+                    produceIr: null,
+                    produceOptPipeline: null,
+                    produceDevice: false,
+                    overrides: [],
+                },
+                filters: {
+                    binaryObject: false,
+                    binary: false,
+                    execute: true,
+                    intel: true,
+                    demangle: true,
+                    labels: true,
+                    directives: true,
+                    commentOnly: true,
+                    trim: false,
+                    debugCalls: false,
+                },
+                tools: [],
+                libraries: [],
+                executeParameters: {
+                    args: "",
+                    stdin: "",
                 },
             },
         })
-        .json()) as {
+    ).data as {
         execResult: {
             stdout: { text: string }[];
             stderr: { text: string }[];
